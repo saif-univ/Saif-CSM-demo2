b92a324d95dacb92b8214e8c08a1acbe77b76ab5
saif-univ <saifalli@univlabs.in>
2025-01-16 15:45:46 +0530

    fix7

--- src/app/ui/surgical-session/guidance-session/sidebar/CapturedImagesList.tsx
+++ src/app/ui/surgical-session/guidance-session/sidebar/CapturedImagesList.tsx
@@ -1,311 +1,7 @@
 import React, { useState } from "react";
 import { ImageMetadata } from "@/app/types/imageTypes";
 import { TrashIcon } from "@heroicons/react/24/outline";
-const imgData = [
-    {
-        filename: "img1.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T050921224Z",
-    },
-    {
-        filename: "surgery_20241127T051117336Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T051117336Z",
-    },
-    {
-        filename: "surgery_20241127T052713675Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T052713675Z",
-    },
-    {
-        filename: "surgery_20241127T053245666Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T053245666Z",
-    },
-    {
-        filename: "surgery_20241127T054154431Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T054154431Z",
-    },
-    {
-        filename: "surgery_20241127T054304128Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T054304128Z",
-    },
-    {
-        filename: "surgery_20241127T065120733Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T065120733Z",
-    },
-    {
-        filename: "surgery_20241127T065122354Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T065122354Z",
-    },
-    {
-        filename: "surgery_20241127T065125739Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241127T065125739Z",
-    },
-    {
-        filename: "surgery_20241128T154959937Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        timestamp: "20241128T154959937Z",
-    },
-    {
-        filename: "surgery_20241128T175727736Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        timestamp: "20241128T175727736Z",
-    },
-    {
-        filename: "surgery_20241129T052737429Z.jpg",
-        capturedBy: "Sridhar",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T052737429Z",
-    },
-    {
-        filename: "surgery_20241129T052841301Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        timestamp: "20241129T052841301Z",
-    },
-    {
-        filename: "surgery_20241129T100608524Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T100608524Z",
-    },
-    {
-        filename: "surgery_20241129T100624931Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T100624931Z",
-    },
-    {
-        filename: "surgery_20241129T100636585Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T100636585Z",
-    },
-    {
-        filename: "surgery_20241129T100739514Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T100739514Z",
-    },
-    {
-        filename: "surgery_20241129T100805991Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T100805991Z",
-    },
-    {
-        filename: "surgery_20241129T100832322Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T100832322Z",
-    },
-    {
-        filename: "surgery_20241129T100937167Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T100937167Z",
-    },
-    {
-        filename: "surgery_20241129T101006994Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T101006994Z",
-    },
-    {
-        filename: "surgery_20241129T101153684Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T101153684Z",
-    },
-    {
-        filename: "surgery_20241129T101217545Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T101217545Z",
-    },
-    {
-        filename: "surgery_20241129T101449183Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T101449183Z",
-    },
-    {
-        filename: "surgery_20241129T101617388Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T101617388Z",
-    },
-    {
-        filename: "surgery_20241129T102321433Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T102321433Z",
-    },
-    {
-        filename: "surgery_20241129T102430362Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T102430362Z",
-    },
-    {
-        filename: "surgery_20241129T102526088Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T102526088Z",
-    },
-    {
-        filename: "surgery_20241129T104344385Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T104344385Z",
-    },
-    {
-        filename: "surgery_20241129T133929442Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241129T133929442Z",
-    },
-    {
-        filename: "surgery_20241129T134133293Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        timestamp: "20241129T134133293Z",
-    },
-    {
-        filename: "surgery_20241129T134200189Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        timestamp: "20241129T134200189Z",
-    },
-    {
-        filename: "surgery_20241201T163438625Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        timestamp: "20241201T163438625Z",
-    },
-    {
-        filename: "surgery_20241202T072338558Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241202T072338558Z",
-    },
-    {
-        filename: "surgery_20241204T062332010Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241204T062332010Z",
-    },
-    {
-        filename: "surgery_20241204T062354211Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        capturedBy: "Dr. John Doe",
-        timestamp: "20241204T062354211Z",
-    },
-    {
-        filename: "surgery_20241204T103606087Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        capturedBy: "Dr. John Doe",
-        timestamp: "20241204T103606087Z",
-    },
-    {
-        filename: "surgery_20241204T105256449Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241204T105256449Z",
-    },
-    {
-        filename: "surgery_20241205T043908662Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241205T043908662Z",
-    },
-    {
-        filename: "surgery_20241205T043938980Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241205T043938980Z",
-    },
-    {
-        filename: "surgery_20241205T094821817Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241205T094821817Z",
-    },
-    {
-        filename: "surgery_20241205T095112349Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241205T095112349Z",
-    },
-    {
-        filename: "surgery_20241205T095540032Z.jpg",
-        capturedBy: "Dr. John Doe",
-        sessionId: "surgery123",
-        description: "Captured image from surgery session",
-        timestamp: "20241205T095540032Z",
-    },
-    {
-        filename: "surgery_20241205T100201587Z.jpg",
-        sessionId: "surgery123",
-        description: "Annotated image",
-        capturedBy: "Dr. John Doe",
-        timestamp: "20241205T100201587Z",
-    },
-];
-const singleImg = require("./CapturedImages/img1.jpg")
+
 
 interface CapturedImagesListProps {
     images: ImageMetadata[];
@@ -323,9 +19,9 @@ const CapturedImagesList: React.FC<CapturedImagesListProps> = ({
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 12; // Increased for better grid layout
 
-    const totalPages = Math.ceil(imgData.length / itemsPerPage);
+    const totalPages = Math.ceil(images.length / itemsPerPage);
     const startIndex = (currentPage - 1) * itemsPerPage;
-    const paginatedImages = imgData
+    const paginatedImages = images
         .sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime())
         .toReversed()
         .slice(startIndex, startIndex + itemsPerPage);
@@ -333,9 +29,6 @@ const CapturedImagesList: React.FC<CapturedImagesListProps> = ({
     const handlePageChange = (page: number) => {
         setCurrentPage(page);
     };
-
-    console.log(imgData, "imgDataimgDataimgDataimgData", singleImg);
-
 
     return (
         <div className="flex flex-col h-full w-full">
@@ -352,14 +45,12 @@ const CapturedImagesList: React.FC<CapturedImagesListProps> = ({
                         {/* Image Thumbnail */}
                         <div>
                             <img
-                                // src={`/surgical-session/surgery123/images/img1.jpg`}
-                                src={`/surgical-session/surgery123/images/${image.filename}`}
-                                // src={singleImg}
+                                src={`/surgical-session/surger123/images/${image.filename}`}
                                 alt={image.description}
                                 onClick={() => onImageSelect(image)}
                                 className="w-full h-24 object-cover cursor-pointer"
                             />
-                            okokkok
+
                             {/* Delete Button - Appears on Hover */}
                             <button
                                 className="absolute top-2 right-2 p-1.5 bg-red-500 text-white rounded-full opacity-0 group-hover:opacity-100 transition-opacity"
